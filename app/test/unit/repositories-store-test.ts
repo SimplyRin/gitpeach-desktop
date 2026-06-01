31ec4c70e1aebff1f9c5784a4d2fab6603de7b6c
Alex Ruddick <aruddick@copioa.io>
2026-06-01 12:34:21 -0500

    Tear down TestRepositoriesDatabase properly

--- app/test/unit/repositories-store-test.ts
+++ app/test/unit/repositories-store-test.ts
@@ -1,4 +1,4 @@
-import { describe, it, beforeEach } from 'node:test'
+import { describe, it, beforeEach, afterEach } from 'node:test'
 import assert from 'node:assert'
 import { RepositoriesStore } from '../../src/lib/stores/repositories-store'
 import { TestRepositoriesDatabase } from '../helpers/databases'
@@ -13,6 +13,10 @@ describe('RepositoriesStore', () => {
     repoDb = new TestRepositoriesDatabase()
     await repoDb.reset()
     repositoriesStore = new RepositoriesStore(repoDb)
+  })
+
+  afterEach(() => {
+    repoDb.close()
   })
 
   describe('adding a new repository', () => {
